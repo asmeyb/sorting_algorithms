6b074818c3c85317a1088c710359f689e3c66ee6
asmeyb <asmeb@gmail.com>
2021-10-25 01:18:12 -0700

    Bubble_sort function & O notation for bubble-array-sort

--- sort.h
+++ sort.h
@@ -24,4 +24,6 @@ typedef listint_t l_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+void bubble_sort(int *array, size_t size);
+
 #endif /* SORT.H */
